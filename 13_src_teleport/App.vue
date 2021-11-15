b1d63cb285c5f375eec3a921ea91404ea7061157
masshio <2641711397@qq.com>
2021-11-16 00:05:50 +0800

    teleport

---
+++ 13_src_teleport/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <div>我是app</div>
+    <son></son>
+  </div>
+</template>
+
+<script>
+import Son from './components/Son'
+import {provide, reactive, toRefs} from 'vue'
+export default {
+  name: "App",
+  setup() {
+    const data = reactive({
+      x: 1,
+      y: 2
+    })
+    provide('data', data)
+    return{
+      ...toRefs(data)
+    }
+  },
+  components: {
+    Son
+  }
+};
+</script>
+
+<style>
+  .app {
+    background: yellowgreen;
+    padding: 20px;
+  }
+</style>
---
+++ 13_src_teleport/components/GrandSon.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="grand">
+    我是GrandSon {{ data }}
+    <my-dialog></my-dialog>
+  </div>
+  
+</template>
+
+<script>
+import { inject, reactive, toRefs } from "vue";
+import MyDialog from './MyDialog.vue'
+export default {
+  name: "App",
+  setup() {
+    let data = inject("data");
+    return {
+      data,
+    };
+  },
+  components: {
+    MyDialog
+  }
+};
+</script>
+
+<style>
+.grand {
+  background: wheat;
+  padding: 20px;
+}
+</style>
