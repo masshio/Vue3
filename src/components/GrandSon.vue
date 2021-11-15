f7fc6f9ed0e4a4ba836e630fc29e291becc4a953
masshio <2641711397@qq.com>
2021-11-15 16:44:43 +0800

    provide/inject

---
+++ src/components/GrandSon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="grand">我是GrandSon {{data}}</div>
+</template>
+
+<script>
+import { inject, reactive, toRefs } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let data = inject("data");
+    return {
+        data
+    }
+  },
+};
+</script>
+
+<style>
+.grand {
+  background:wheat;
+  padding: 20px;
+}
+</style>
---
+++ src/components/Son.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="son">
+      我是son{{data}}
+      <grand-son></grand-son>
+  </div>
+</template>
+
+<script>
+import GrandSon from "./GrandSon";
+import { inject, reactive, toRefs } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let data = inject("data");
+    return {
+      data,
+    };
+  },
+  components: {
+    GrandSon,
+  },
+};
+</script>
+
+<style>
+.son {
+  background: yellow;
+  padding: 20px;
+}
+</style>
